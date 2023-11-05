6f2325cba89f2b3193995a32f27a473ceac1cd89
frs11~ <fayezurrahman017@gmail.com>
2023-11-05 20:58:09 +0600

    created context and sign in information. Also created .env file

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,12 @@ import "./index.css";
 import * as ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import customRoutes from "./Routes/Routes";
+import AuthProvider from "./Contexts/AuthProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={customRoutes} />
+    <AuthProvider>
+      <RouterProvider router={customRoutes} />
+    </AuthProvider>
   </React.StrictMode>
 );
