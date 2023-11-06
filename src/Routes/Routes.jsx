4b7b8709e4b703e132b54a4de3717d6098f5520e
frs11~ <fayezurrahman017@gmail.com>
2023-11-06 15:13:17 +0600

    Added registration and Login page. Also Setup Private route page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../pages/Layout";
+import Registration from "../pages/Registration";
+import Login from "../pages/Login";
 
 const customRoutes = createBrowserRouter([
   {
     path: "/",
     element: <Layout></Layout>,
+    children: [
+      {
+        path: "/registration",
+        element: <Registration></Registration>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
   },
 ]);
 
